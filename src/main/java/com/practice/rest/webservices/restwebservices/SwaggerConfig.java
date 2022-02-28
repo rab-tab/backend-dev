ef6ae42e03c1430c4cb3afb06dd21ba9fc1033fc
Rabia <sdetcode@gmail.com>
2022-02-28 22:25:26 +0530

    swagger config changes

--- src/main/java/com/practice/rest/webservices/restwebservices/SwaggerConfig.java
+++ src/main/java/com/practice/rest/webservices/restwebservices/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.practice.rest.webservices.restwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,6 +16,10 @@ public class SwaggerConfig {
     @Bean
     public Docket api()
     {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
