24d905c5e80b4580bd0109c7a58099117829bdad
Rabia <sdetcode@gmail.com>
2022-03-01 15:11:22 +0530

    data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into user values(1,sysdate(),'XY');
-insert into user values(2,sysdate(),'AB');
+insert into user values(10001,sysdate(),'XY');
+insert into user values(10002,sysdate(),'AB');
