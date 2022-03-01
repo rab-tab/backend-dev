940f750e2f963ecd83f8c0295cf782ea2d678a7a
Rabia <sdetcode@gmail.com>
2022-03-01 13:36:49 +0530

    jpa connection setup

--- src/main/java/com/practice/rest/webservices/restwebservices/user/User.java
+++ src/main/java/com/practice/rest/webservices/restwebservices/user/User.java
@@ -1,10 +1,19 @@
 package com.practice.rest.webservices.restwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2,message = "Name should have atleast two characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user values(1,sysdate(),'XY');
+insert into user values(2,sysdate(),'AB');
